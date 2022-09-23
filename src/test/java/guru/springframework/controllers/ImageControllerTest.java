4259610eb448fc6adbf49b4b8b3447e8830a1e71
Ihor Horbonos <igorgorb@gmail.com>
2022-09-23 15:25:46 +0300

    added features show saved image from db

--- src/test/java/guru/springframework/controllers/ImageControllerTest.java
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -3,6 +3,8 @@ package guru.springframework.controllers;
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.ImageService;
 import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -13,6 +15,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -74,5 +77,35 @@ public class ImageControllerTest {
       .andExpect(header().string("Location", "/recipe/1/show"));
 
     verify(imageService, times(1)).saveImageFile(anyLong(), any());
+  }
+
+  @Test
+  public void renderImageFromDB() throws Exception {
+    //given
+    RecipeCommand command = new RecipeCommand();
+    command.setId(1L);
+
+    String s = "fake image text";
+    Byte[] byteObject = new Byte[s.getBytes().length];
+
+    int i = 0;
+
+    for (byte b : s.getBytes()) {
+      byteObject[i++] = b;
+    }
+
+    command.setImage(byteObject);
+
+    when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+    //when
+    MockHttpServletResponse response
+      = mockMvc.perform(get("/recipe/1/recipeimage"))
+        .andExpect(status().isOk())
+        .andReturn().getResponse();
+
+    byte[] responseBytes = response.getContentAsByteArray();
+
+    assertEquals(s.getBytes().length, responseBytes.length);
   }
 }
